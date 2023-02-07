d9cbe3841798e3a67343ee9bd005b519fdb8203e
Douglas <ykinggutenberg@gmail.com>
2023-02-06 21:49:16 -0300

    gerador de nome de banda

---
+++ Day1/projeto1.py
@@ -0,0 +1,6 @@
+#um gerador de nome para bandas
+print("Welcome to the band name Generator.")
+questone = input("Whats name of the city you grew up in?\n")
+questtwo = input("whats your pets name?\n")
+band = "Your band name could be "
+print(band + questone + ' ' + questtwo)
